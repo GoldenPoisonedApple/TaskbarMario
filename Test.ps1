b8aae9b746a906ee01492b9dd030b32d70b982a9
ito <nanisiyou1@gmail.com>
2024-03-20 22:47:42 +0900

    #1 一人が出来た

--- Test.ps1
+++ Test.ps1
@@ -2,6 +2,9 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 
+$PYPE = "img/pipe.png"
+$TASKBAR_HEIGHT = 35
+
 class TypeConverter {
 	static [type] GetType([string] $typeName) {
 		return $typeName -as [type]
@@ -12,16 +15,24 @@ class Mario {
 	# タスクバーの高さ
 	static $TASKBAR_HEIGHT = 35
 	# 画像のパスを指定
-	static $imagePath = "img/1.png"
+	static [string[]] $imagePath = @("img/1.png", "img/2.png", "img/3.png", "img/4.png")
+
 	# ウィンドウ
 	$form
+	# ピクチャーボックス
+	$pictureBox
+	# 画像
+	$images = @()
+	# 画面の幅
+	$screen_x
+	# 画像のインデックス
+	$index = 0
 
 	Mario() {
 		$this.Create()
 	}
 
 	[void] Create () {
-
 		# ウィンドウの作成
 		$this.form = New-Object System.Windows.Forms.Form
 		# ControlBox プロパティを使って"×"マークを非表示にする
@@ -30,6 +41,11 @@ class Mario {
 		$this.form.ShowInTaskbar = $false
 		# ウィンドウの境界スタイルを設定
 		$this.form.FormBorderStyle = [TypeConverter]::GetType("System.Windows.Forms.FormBorderStyle")::None
+		# ウィンドウを透明にする
+		$this.form.TransparencyKey = $this.form.BackColor
+		# カーソルをデフォルトに設定
+		#$this.form.Cursor = [TypeConverter]::GetType("System.Windows.Forms.Cursors")::Cross
+  
 
 		# ウィンドウのサイズを設定
 		# 136 x 50 が最小サイズ
@@ -39,33 +55,97 @@ class Mario {
 		# 50 x 50 だと画像がはみ出るので 45 x 45
 		$this.form.ClientSize = New-Object System.Drawing.Size(45, 45)
 
-		# 画像の読み込み
-		$image = [TypeConverter]::GetType("System.Drawing.Image")::FromFile($this::imagePath)
+		# 透過処理済みの画像を読み込み
+		for ($i = 0; $i -lt $this::imagePath.Count; $i++) {
+			$this.images += [TypeConverter]::GetType("System.Drawing.Image")::FromFile($this::imagePath[$i])
+		}
 		# ピクチャーボックスの作成
-		$pictureBox = New-Object System.Windows.Forms.PictureBox
-		$pictureBox.SizeMode = [TypeConverter]::GetType("System.Windows.Forms.PictureBoxSizeMode")::Zoom
-		$pictureBox.Image = $image
-		$pictureBox.Width = $this.form.ClientSize.Width
-		$pictureBox.Height = $this.form.ClientSize.Height
+		$this.pictureBox = New-Object System.Windows.Forms.PictureBox
+		$this.pictureBox.SizeMode = [TypeConverter]::GetType("System.Windows.Forms.PictureBoxSizeMode")::Zoom
+		$this.pictureBox.Width = $this.form.ClientSize.Width
+		$this.pictureBox.Height = $this.form.ClientSize.Height
+		# 画像を表示
+		$this.ChangeImage()
 		# ピクチャーボックスをフォームに追加
-		$this.form.Controls.Add($pictureBox)
+		$this.form.Controls.Add($this.pictureBox)
 
 		# 画面の中央にウィンドウを配置
 		$screen = [TypeConverter]::GetType("System.Windows.Forms.Screen")::PrimaryScreen
-		$screen_x = $screen.Bounds.Width
+		$this.screen_x = $screen.Bounds.Width - $this.form.Width
+		Write-Host "Screen Width: $($this.screen_x)"
 		$screen_y = $screen.Bounds.Height - $this::TASKBAR_HEIGHT
-		$form_width = $this.form.Width
-		$form_height = $this.form.Height
 		$this.form.StartPosition = "Manual"
-		$this.form.Left = $screen_x - $form_width
-		$this.form.Top = $screen_y - $form_height
+		$this.form.Left = 0
+		$this.form.Top = $screen_y - $this.form.Height
 	}
 
 	[void] Show() {
 		# ウィンドウを表示
-		$this.form.ShowDialog()
+		$this.form.Show();
+		# マリオを動かす
+		while ($true) {
+			# 移動
+			$this.form.Left += 20
+			Write-Host "Left: $($this.form.Left)"
+			# 画像の切り替え
+			$this.ChangeImage()
+
+			# 画面外に出たら左端に戻る
+			if ($this.form.Left -ge $this.screen_x) {
+				Write-Host "To the left end."
+				# 左端に戻る
+				$this.form.Left = 0
+			}
+
+			# ウェイト時間
+			Start-Sleep -Milliseconds 100
+		}
+	}
+
+	[void] ChangeImage() {
+		$this.index++
+		if ($this.index -ge $this::imagePath.Length) {
+			$this.index = 0
+		}
+		# Write-Host "ChangeImage: $($this.index)"
+		$this.pictureBox.Image = $this.images[$this.index]
+		$this.form.Refresh()
 	}
 }
+
+function PutClayPipe () {
+	# ウィンドウの作成
+	$form = New-Object System.Windows.Forms.Form
+	# ControlBox プロパティを使って"×"マークを非表示にする
+	$form.ControlBox = $false
+	# タスクバーに表示しないようにする
+	$form.ShowInTaskbar = $false
+	# ウィンドウの境界スタイルを設定
+	$form.FormBorderStyle = [TypeConverter]::GetType("System.Windows.Forms.FormBorderStyle")::None
+	# ウィンドウを透明にする
+	$form.TransparencyKey = $form.BackColor
+	# 大きさを設定
+	$form.Size = New-Object System.Drawing.Size(120, 80)
+	$form.ClientSize = New-Object System.Drawing.Size(120, 80)
+	# ピクチャーボックスの作成
+	$pictureBox = New-Object System.Windows.Forms.PictureBox
+	$pictureBox.SizeMode = [TypeConverter]::GetType("System.Windows.Forms.PictureBoxSizeMode")::Zoom
+	$pictureBox.Width = $form.ClientSize.Width
+	$pictureBox.Height = $form.ClientSize.Height
+	$pictureBox.Image = [TypeConverter]::GetType("System.Drawing.Image")::FromFile($PYPE)
+	# ピクチャーボックスをフォームに追加
+	$form.Controls.Add($pictureBox)
+	# 位置を設定して表示
+	$form.StartPosition = "Manual"
+	$screen = [TypeConverter]::GetType("System.Windows.Forms.Screen")::PrimaryScreen
+	$form.Left = $screen.Bounds.Width - $form.Width
+	$form.Top = $screen.Bounds.Height - $form.Height - $TASKBAR_HEIGHT
+	# ウィンドウを表示
+	$form.Show()
+}
+
+# パイプを表示
+PutClayPipe
 
 # マリオを表示
 $mario = [Mario]::new()
--- Test2.ps1
+++ Test2.ps1
@@ -1,54 +1,28 @@
 Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
 
-
-# タスクバーの高さ
-$TASKBAR_HEIGHT = 35
 # 画像のパスを指定
-$imagePath = "img/1.png"
+$imagePaths = @("img/1.png", "img/2.png", "img/3.png", "img/4.png")
 
-# ウィンドウの作成
-$form = New-Object System.Windows.Forms.Form
-# ControlBox プロパティを使って"×"マークを非表示にする
-$form.ControlBox = $false
-# タスクバーに表示しないようにする
-$form.ShowInTaskbar = $false
-# ウィンドウの境界スタイルを設定
-$sys_windows_forms_formBorderstyle = "System.Windows.Forms.FormBorderStyle" -as [type]
-$form.FormBorderStyle = $sys_windows_forms_formBorderstyle::None
+# フォームを作成
+$form = New-Object Windows.Forms.Form
+$form.Text = "Animated Image Viewer"
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+$form.StartPosition = "CenterScreen"
+$form.Size = New-Object System.Drawing.Size(400, 300)
 
-# ウィンドウのサイズを設定
-# 136 x 50 が最小サイズ
-$form.MinimumSize = New-Object System.Drawing.Size(136, 50)
-$form.Size = New-Object System.Drawing.Size(136, 50)
-# クライアント領域のサイズを設定
-# 50 x 50 だと画像がはみ出るので 45 x 45
-$form.ClientSize = New-Object System.Drawing.Size(45, 45)
-
-# 画像の読み込み
-$sys_drawing_image = "System.Drawing.Image" -as [type]
-$image = $sys_drawing_image::FromFile($imagePath)
-# ピクチャーボックスの作成
-$pictureBox = New-Object System.Windows.Forms.PictureBox
-		
-$pictureBox.SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::Zoom
-$pictureBox.Image = $image
-$pictureBox.Width = $form.ClientSize.Width
-$pictureBox.Height = $form.ClientSize.Height
-# ピクチャーボックスをフォームに追加
+# PictureBoxを作成
+$pictureBox = New-Object Windows.Forms.PictureBox
+$pictureBox.Dock = [System.Windows.Forms.DockStyle]::Fill
 $form.Controls.Add($pictureBox)
+# フォームを表示
+$form.Show() | Out-Null
 
-# 画面の中央にウィンドウを配置
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen
-$screen_x = $screen.Bounds.Width
-$screen_y = $screen.Bounds.Height - $TASKBAR_HEIGHT
-$form_width = $form.Width
-$form_height = $form.Height
-$form.StartPosition = "Manual"
-$form.Left = $screen_x - $form_width
-$form.Top = $screen_y - $form_height
-
-
-
-# ウィンドウを表示
-$form.ShowDialog()
+while ($true) {
+	foreach ($path in $imagePaths) {
+		Write-Host "Switching Image to $path"
+			$image = [System.Drawing.Image]::FromFile($path)
+			$pictureBox.Image = $image
+			$form.Refresh()
+			Start-Sleep -Milliseconds 100
+	}
+}
