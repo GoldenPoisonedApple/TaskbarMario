7001626ce4fce95e152f12521485ce04b9007c24
ito <nanisiyou1@gmail.com>
2024-03-20 18:34:19 +0900

    ウィンドウの大きさ実装

--- Test.ps1
+++ Test.ps1
@@ -1,24 +1,34 @@
 Add-Type -AssemblyName System.Windows.Forms
 
+# タスクバーの高さ
+$TASKBAR_HEIGHT = 35
+
 # ウィンドウの作成
 $form = New-Object System.Windows.Forms.Form
-$form.Size = New-Object System.Drawing.Size(50, 50)  # ウィンドウのサイズを設定
+# ControlBox プロパティを使って"×"マークを非表示にする
+$form.ControlBox = $false
+# ウィンドウのサイズを設定
+# 136 x 50 が最小サイズ
+$form.MinimumSize = New-Object System.Drawing.Size(136, 50)
+$form.Size = New-Object System.Drawing.Size(136, 50)
+# ウィンドウの境界スタイルを設定
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+
+
 
 # 画面の中央にウィンドウを配置
 $screen = [System.Windows.Forms.Screen]::PrimaryScreen
 $screen_x = $screen.Bounds.Width
-$screen_y = $screen.Bounds.Height
+$screen_y = $screen.Bounds.Height - $TASKBAR_HEIGHT
 Write-Host "Screen size: $screen_x x $screen_y"
 $form_width = $form.Width
 $form_height = $form.Height
 Write-Host "Form size: $form_width x $form_height"
 
+# ウィンドウの位置を設定
 $form.StartPosition = "Manual"
-
 $form.Left = $screen_x - $form_width
 $form.Top = $screen_y - $form_height
-# $form.Left = 0
-# $form.Top = 0
 
 # ウィンドウを表示
 $form.ShowDialog()
